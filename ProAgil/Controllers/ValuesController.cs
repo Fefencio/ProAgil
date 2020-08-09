eeb867604f5a53ea566ae2c56a7de44f42be06f7
fernando.mauricio <fernando.mauricio@APREVDES42>
2020-08-09 03:59:25 +0100

    Add project files.

---
+++ ProAgil/Controllers/ValuesController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProAgil.Data;
+using ProAgil.Models;
+
+namespace ProAgil.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context )
+        {
+            this._context = context;
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var eventos = await _context.Eventos.ToListAsync();
+                return Ok(eventos);
+            }
+            catch
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var evento = await _context.Eventos.FirstOrDefaultAsync(x => x.EventoId == id);
+                return Ok(evento);
+            }
+            catch
+            {
+                return this.StatusCode(StatusCodes.Status501NotImplemented, "Um erro qualquer de teste");
+            }
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ProAgil/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProAgil.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProAgil.Data
+{
+    public class DataContext: DbContext
+    {
+        public DbSet<Evento> Eventos { get; set; }
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+
+        }
+    }
+}
